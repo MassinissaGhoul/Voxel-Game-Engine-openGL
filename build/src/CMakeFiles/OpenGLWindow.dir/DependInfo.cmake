1785586daadc1f7fef5bc929d098fc5b4a2470d6
MassinissaGhoul <massigh680@gmail.com>
2025-01-18 17:38:31 +0100

    init projet

---
+++ build/src/CMakeFiles/OpenGLWindow.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/massi/devRepo/RiCopengl/src/glad/src/glad.c" "src/CMakeFiles/OpenGLWindow.dir/glad/src/glad.c.o" "gcc" "src/CMakeFiles/OpenGLWindow.dir/glad/src/glad.c.o.d"
+  "/home/massi/devRepo/RiCopengl/src/main.cpp" "src/CMakeFiles/OpenGLWindow.dir/main.cpp.o" "gcc" "src/CMakeFiles/OpenGLWindow.dir/main.cpp.o.d"
+  "" "src/OpenGLWindow" "gcc" "src/CMakeFiles/OpenGLWindow.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
