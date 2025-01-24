728429d873531d22623a1c0c121052b80c50d63f
MassinissaGhoul <massigh680@gmail.com>
2025-01-24 19:41:21 +0100

    class shader

---
+++ src/shader.cpp
@@ -0,0 +1,95 @@
+#include "shader.h"
+#include <fstream>
+
+Shader::Shader(const char *vertexPath, const char *fragmentPath) {
+    // ça recup les data des shader de vertex et fragment et on declare les
+    // fichier
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    try {
+        // ça ouvre les fichiers
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+
+        // on inject dans les objets stringstream le contenu des fichiers
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        // close file handlers
+        vShaderFile.close();
+        fShaderFile.close();
+        // convert stream into string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+    } catch (std::ifstream::failure &e) {
+        std::cout << "ERROR::SHADER::FILE_NOT_READ" << e.what() << std::endl;
+    }
+    // ça converti le contenu qui est typé string en typé char compatible note :
+    // (OPENGL est écrit en C)
+    const char *vShaderCode = vertexCode.c_str();
+    const char *fShaderCode = fragmentCode.c_str();
+    unsigned int vertex, fragment;
+    // crée lie et compile le vertex shader
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+    // classicshit pour le fragment shader
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+
+    // ça cree le shader program et on link tout
+    this->ID = glCreateProgram();
+    glAttachShader(this->ID, vertex);
+    glAttachShader(this->ID, fragment);
+    glLinkProgram(ID);
+    checkCompileErrors(this->ID, "PROGRAM");
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+void Shader::use() { glUseProgram(this->ID); }
+
+void Shader::setBool(const std::string &name, bool value) const {
+    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string &name, int value) const {
+    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const {
+    glUniform1f(glGetUniformLocation(this->ID, name.c_str()), value);
+}
+
+void Shader::checkCompileErrors(unsigned int shader, std::string type) {
+    int success;
+    char infoLog[1024];
+    if (type != "PROGRAM") {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            std::cout
+                << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n"
+                << infoLog
+                << "\n -- --------------------------------------------------- "
+                   "-- "
+                << std::endl;
+        }
+    } else {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            std::cout
+                << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n"
+                << infoLog
+                << "\n -- --------------------------------------------------- "
+                   "-- "
+                << std::endl;
+        }
+    }
+}
