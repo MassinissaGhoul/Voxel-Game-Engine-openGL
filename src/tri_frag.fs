d425aaa96ae086dbd1df1ead952d4e61fa2b6852
MassinissaGhoul <massigh680@gmail.com>
2025-01-25 18:30:53 +0100

    texture sur le rectangle

--- src/tri_frag.fs
+++ src/tri_frag.fs
@@ -1,10 +1,13 @@
 #version 330 core
 #extension GL_ARB_separate_shader_objects : enable
-out vec4 FragColor;  
-in vec3 ourColor;
+out vec4 FragColor;
   
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+    FragColor = texture(ourTexture, TexCoord);
 }
-
