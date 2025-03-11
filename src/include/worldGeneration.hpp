f791e7010663a608d24da4adc3f9d3c6b3cbc250
TenixOue <79940202+TenixOue@users.noreply.github.com>
2025-03-11 08:29:14 +0100

    worldGen class update

---
+++ src/include/worldGeneration.hpp
@@ -0,0 +1,17 @@
+#ifndef WORLDGENERATION_HPP
+#define WORLDGENERATION_HPP
+#include "world.hpp"
+class World;
+class WorldGeneration{
+    public:
+
+    WorldGeneration(World& worldRef);
+    ~WorldGeneration();
+    private:
+
+    
+};
+
+
+
+#endif 
---
+++ src/worldGeneration.cpp
@@ -0,0 +1,2 @@
+#include "include/worldGeneration.hpp"
+
